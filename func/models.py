75c17971cfc24fd6167b5e828fb88f85a4ba39c5
Rajat Shenoi <48760956+RajatShenoi@users.noreply.github.com>
2024-12-06 17:23:26 +0530

    third

--- func/models.py
+++ func/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    calories = models.IntegerField()
+    protein = models.IntegerField()
+    fat = models.IntegerField()
+    carbohydrates = models.IntegerField()
+
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
+
+    def __str__(self):
+        return self.name
--- func/urls.py
+++ func/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'func'
 
 urlpatterns = [
+    path("", views.menu, name="order"),
 ]
